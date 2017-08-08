89d5886a487022819e2ff027cf3459027df4488b
O'Riley <david.oriley@cgi.com>
2017-08-07 20:20:33 -0400

    setup app for heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //configure default promise for mongoose
-mongoose.connect('mongodb://localhost:27017/TodoApp'); //connect same way you would with the MongoClient
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'); //connect same way you would with the MongoClient
+//process.env variable is to connect to the mongo database on heroku when it is deployed.
+//used mLab app addon for the database
+//to check the uri value from the cmd type: heroku config --app APP_NAME_HERE
 
 module.exports = {
     mongoose
