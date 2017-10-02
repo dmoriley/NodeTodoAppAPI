56954fb2389ba93176930b0ce28150aa5baf50f0
dmoriley <david.m.oriley@hotmail.com>
2017-10-02 15:28:51 -0400

    converted some function in server to use async await

--- db/config/config.js
+++ db/config/config.js
@@ -8,13 +8,3 @@ if(env === 'development' || env === 'test') {
         process.env[key] = envConfig[key];
     });
 }
-
-//to check the uri value from the cmd type: heroku config --app APP_NAME_HERE
-// if(env === 'development') {
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-// }
-// else if(env === 'test') {
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-// }
